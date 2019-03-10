5de8ee15457bbfa37c9b358a68eef0a44c83e825
debosch <vlad.shiltsin@gmail.com>
2019-03-10 19:04:00 +0200

    Few layouts added, design updated

--- app/src/main/java/com/deboshdaniily/chuckapp/categories/CategoryName.kt
+++ app/src/main/java/com/deboshdaniily/chuckapp/categories/CategoryName.kt
@@ -3,15 +3,14 @@ package com.deboshdaniily.chuckapp.categories
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.deboshdaniily.chuckapp.R
-import kotlinx.android.synthetic.main.category_layout.*
+import kotlinx.android.synthetic.main.activity_new_category.*
 
-class Category : AppCompatActivity() {
+class CategoryName : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.category_layout)
-        title = "Choose category"
-        btn_cancel.setOnClickListener { finish() }
-
+        setContentView(R.layout.activity_category_name)
+        title = ""
+        btn_cancel_new.setOnClickListener { finish() }
     }
 }
