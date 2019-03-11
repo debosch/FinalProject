76356a09f6a58a0e36e420541427fae2ec1bd205
daniily <daniily000@yandex.ru>
2019-03-11 08:37:42 +0200

    JokesAdapter

--- app/src/main/java/com/deboshdaniily/chuckapp/MainActivity.kt
+++ app/src/main/java/com/deboshdaniily/chuckapp/MainActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.deboshdaniily.chuckapp.categories.Category
 import com.deboshdaniily.chuckapp.data.DataService
 import com.deboshdaniily.chuckapp.data.DataServiceImpl
-import com.deboshdaniily.chuckapp.categories.Category
 import com.deboshdaniily.chuckapp.jokes.NewJoke
+import com.deboshdaniily.chuckapp.ui.JokeAdapder
 import kotlinx.android.synthetic.main.activity_main.*
 
+private const val JOKES_LIMIT = 50
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,19 +33,37 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        joke_list.adapter = JokeAdapder(JOKES_LIMIT) { _, callback ->
+            service.getRandomJoke(callback::invoke)
+        }
+        joke_list.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.toolbar_menu, menu)
 
-        menu!!.findItem(R.id.categories_menu_item).setOnMenuItemClickListener {
+        menu?.findItem(R.id.categories_menu_item)?.setOnMenuItemClickListener {
             val intent = Intent(this, Category::class.java)
             startActivity(intent)
-            return@setOnMenuItemClickListener false
+            true
         }
 
+        menu?.findItem(R.id.saved_menu_item)?.setOnMenuItemClickListener {
+            service.getCachedJokesCount {
+                if (it.isSuccess) {
+                    val count = it.get()
+                    runOnUiThread {
+                        joke_list.adapter = JokeAdapder(count) { position, callback ->
+                            service.getCachedJokeById(position + 1) { runOnUiThread { callback.invoke(it) } }
+                        }
+                    }
+                } else {
+                    // TODO debosh: could not get database, show splash screen mb?
+                }
+            }
+            true
+        }
         return super.onCreateOptionsMenu(menu)
     }
-
 }
